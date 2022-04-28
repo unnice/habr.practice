f37f4f95011bb0250cce06d158ed22b8a47af74f
unnice <odinok1y@mail.ru>
2022-04-29 00:27:20 +0300

    annotations 28.04

--- src/MyAnnotation/AnnotationClass.java
+++ src/MyAnnotation/AnnotationClass.java
@@ -1,21 +1,61 @@
 package MyAnnotation;
 
+import java.lang.annotation.*;
+import java.util.List;
+
 public class AnnotationClass {
 
     public static void main(String[] args) {
 
+
+
     }
 }
 
+@MyPreamble(
+        Author = "Leha",
+        version = 0.9f,
+        CheckList = {"Elena", "Vasiliy", "Ivan"}
+)
 abstract class  Parent {
-    public abstract void printName();
+    public void printName(){
+        System.out.println("My name is");
+    }
 }
 
 class Child extends Parent{
 
-
+    @Override
     public  void printName(){
         System.out.println("MyName Is child");
     }
+
+
 }
 
+@Documented
+@interface MyPreamble {
+    String Author();
+    double version();
+    String[] CheckList();
+}
+
+
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@interface JsonSerialazible{
+}
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+@interface JsonElement{
+     String key() default "";
+}
+
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@interface Init{
+
+}
